f27cceab8f97c4977f9dceec31fa31a10ae00a3b
qbhy <qbhy0715@qq.com>
2021-09-24 11:42:51 +0800

    feat: 取消echo依赖

--- http.go
+++ http.go
@@ -1,15 +1,96 @@
 package contracts
 
-import "github.com/labstack/echo/v4"
+import (
+	"mime/multipart"
+	"net/http"
+	"net/url"
+)
 
 // HttpResponse http 响应
 type HttpResponse interface {
 	Status() int
-	Response(ctx echo.Context) error
+	Response(request HttpRequest) error
 }
 
 // HttpRequest http 请求
 type HttpRequest interface {
-	echo.Context
+
+	// Request returns `*http.Request`.
+	Request() *http.Request
+
+	// SetRequest sets `*http.Request`.
+	SetRequest(r *http.Request)
+
+	// IsTLS returns true if HTTP connection is TLS otherwise false.
+	IsTLS() bool
+
+	// IsWebSocket returns true if HTTP connection is WebSocket otherwise false.
+	IsWebSocket() bool
+
+	// Scheme returns the HTTP protocol scheme, `http` or `https`.
+	Scheme() string
+
+	// RealIP returns the client's network address based on `X-Forwarded-For`
+	// or `X-Real-IP` request header.
+	// The behavior can be configured using `Echo#IPExtractor`.
+	RealIP() string
+
+	// Path returns the registered path for the handler.
+	Path() string
+
+	// SetPath sets the registered path for the handler.
+	SetPath(p string)
+
+	// Param returns path parameter by name.
+	Param(name string) string
+
+	// ParamNames returns path parameter names.
+	ParamNames() []string
+
+	// SetParamNames sets path parameter names.
+	SetParamNames(names ...string)
+
+	// ParamValues returns path parameter values.
+	ParamValues() []string
+
+	// SetParamValues sets path parameter values.
+	SetParamValues(values ...string)
+
+	// QueryParam returns the query param for the provided name.
+	QueryParam(name string) string
+
+	// QueryParams returns the query parameters as `url.Values`.
+	QueryParams() url.Values
+
+	// QueryString returns the URL query string.
+	QueryString() string
+
+	// FormValue returns the form field value for the provided name.
+	FormValue(name string) string
+
+	// FormParams returns the form parameters as `url.Values`.
+	FormParams() (url.Values, error)
+
+	// FormFile returns the multipart form file for the provided name.
+	FormFile(name string) (*multipart.FileHeader, error)
+
+	// MultipartForm returns the multipart form.
+	MultipartForm() (*multipart.Form, error)
+
+	// Cookie returns the named cookie provided in the request.
+	Cookie(name string) (*http.Cookie, error)
+
+	// SetCookie adds a `Set-Cookie` header in HTTP response.
+	SetCookie(cookie *http.Cookie)
+
+	// Cookies returns the HTTP cookies sent with the request.
+	Cookies() []*http.Cookie
+
+	// Get retrieves data from the context.
+	Get(key string) interface{}
+
+	// Set saves data in the context.
+	Set(key string, val interface{})
+
 	All() Fields
 }
