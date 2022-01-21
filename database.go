dc21d8d8d71c42a2036fff7f89fd06ec6b137fd0
qbhy <96qbhy@gmail.com>
2022-01-21 17:55:57 +0800

    feat: 更新数据库连接接口

--- database.go
+++ database.go
@@ -19,6 +19,7 @@ type DBTx interface {
 }
 
 type SqlExecutor interface {
+	Query() ([]Fields, error)
 	Get(dest interface{}, query string, args ...interface{}) error
 	Select(dest interface{}, query string, args ...interface{}) error
 	Exec(query string, args ...interface{}) (Result, error)
