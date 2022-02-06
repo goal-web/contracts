96a71c80246528d36694b2577f8276c2d1aeb576
qbhy <96qbhy@gmail.com>
2022-02-06 22:44:04 +0800

    feat: 初步完成队列接口

--- queue.go
+++ queue.go
@@ -4,32 +4,39 @@ type QueueFactory interface {
 	Connection(name ...string) Queue
 }
 
-type QueueDriver func(config Fields) Queue
+type Msg struct {
+	Ack Ack
+	Job Job
+}
+
+type Ack func()
+
+type QueueDriver func(name string, config Fields, serializer JobSerializer) Queue
 
 type Queue interface {
 	// Size Get the size of the queue.
-
 	Size() int64
+
 	// Push a new job onto the queue.
-	Push(job Job, data interface{}, queue ...string)
+	Push(job Job, queue ...string)
 
 	// PushOn Push a new job onto the queue.
-	PushOn(queue string, job Job, data interface{})
+	PushOn(queue string, job Job)
 
 	// PushRaw Push a raw payload onto the queue.
 	PushRaw(payload, queue string, options ...Fields)
 
 	// Later Push a new job onto the queue after a delay.
-	Later(delay interface{}, job Job, data interface{}, queue ...string)
+	Later(delay interface{}, job Job, queue ...string)
 
 	// LaterOn Push a new job onto the queue after a delay.
-	LaterOn(queue string, delay interface{}, job Job, data interface{})
+	LaterOn(queue string, delay interface{}, job Job)
 
 	// Bulk Push an array of jobs onto the queue.
-	Bulk(job Job, data interface{}, queue ...string)
+	Bulk(job Job, queue ...string)
 
 	// Pop the next job off of the queue.
-	Pop(queue ...string) interface{}
+	Pop(queue ...string) Job
 
 	// GetConnectionName Get the connection name for the queue.
 	GetConnectionName() string
@@ -37,7 +44,17 @@ type Queue interface {
 	// SetConnectionName Set the connection name for the queue.
 	SetConnectionName(queue string) Queue
 
-	Listen() chan Job
+	// Release
+	/**
+	 * Release the job back into the queue.
+	 * Accepts a delay specified in seconds.
+	 */
+	Release(job Job, delay ...int)
+
+	// Delete the job from the queue.
+	Delete(job Job)
+
+	Listen(queue ...string) chan Msg
 
 	Stop()
 }
@@ -47,27 +64,14 @@ type Job interface {
 	// Uuid Get the UUID of the job.
 	Uuid() string
 
-	// GetJobId Get the job identifier.
-	GetJobId() string
+	// GetOptions Get the decoded body of the job.
+	GetOptions() Fields
 
-	// Payload Get the decoded body of the job.
-	Payload() Fields
-
-	// Fire the job.
-	Fire()
-
-	// Release
-	/**
-	 * Release the job back into the queue.
-	 * Accepts a delay specified in seconds.
-	 */
-	Release(delay ...int)
+	// Handle the job.
+	Handle()
 
 	// IsReleased Determine if the job was released back into the queue.
 	IsReleased() bool
-
-	// Delete the job from the queue.
-	Delete()
 
 	// IsDeleted Determine if the job has been deleted.
 	IsDeleted() bool
@@ -87,8 +91,14 @@ type Job interface {
 	// Fail Delete the job, call the "failed" method, and raise the failed job event.
 	Fail(err error)
 
-	// MaxTries Get the number of times to attempt a job.
-	MaxTries() int
+	// GetMaxTries Get the max number of times to attempt a job.
+	GetMaxTries() int
+
+	// GetAttemptsNum Get the number of times to attempt a job.
+	GetAttemptsNum() int
+
+	// IncrementAttemptsNum increase the number of attempts
+	IncrementAttemptsNum()
 
 	// MaxExceptions Get the maximum number of exceptions allowed, regardless of attempts.
 	MaxExceptions() int
@@ -99,29 +109,21 @@ type Job interface {
 	// RetryUntil Get the timestamp indicating when the job should timeout.
 	RetryUntil() int
 
-	// GetName Get the name of the queued job class.
-	GetName() string
-
-	// ResolveName
-	/**
-	 * Get the resolved name of the queued job class.
-	 *
-	 * Resolves the name of "wrapped" jobs such as class-based handlers.
-	 */
-	ResolveName() string
-
 	// GetConnectionName Get the name of the connection the job belongs to.
 	GetConnectionName() string
 
 	// GetQueue Get the name of the queue the job belongs to.
 	GetQueue() string
-
-	// GetRawBody Get the raw body string for the job.
-	GetRawBody() string
 }
 
 type QueueWorker interface {
-	Work(queue Queue)
+	Work()
+	Stop()
+}
+
+type JobSerializer interface {
+	Serializer(job Job) string
+	Unserialize(serialized string) (Job, error)
 }
 
 type ShouldQueue interface {
