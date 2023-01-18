6563b9e79f6beac3435252f4f98cb7a3c68ec3b0
qbhy <qbhy0715@qq.com>
2023-01-18 15:22:54 +0800

    faet: 优化配置接口

--- config.go
+++ config.go
@@ -14,10 +14,6 @@ type Config interface {
 	// reload configuration based on given field provider.
 	Reload()
 
-	// Merge 合并给定的配置值
-	// merge the given configuration values.
-	Merge(key string, config Config)
-
 	// Get 获取指定的配置值
 	// get the specified configuration value.
 	Get(key string, defaultValue ...interface{}) interface{}
@@ -29,10 +25,6 @@ type Config interface {
 	// Unset 销毁指定的配置值
 	// Destroy the specified configuration value.
 	Unset(key string)
-
-	// GetConfig 获取指定的配置实例
-	// get the specified configuration instance.
-	GetConfig(key string) Config
 }
 
 type Env interface {
